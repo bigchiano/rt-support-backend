683a0a1b8a58569553f5848e3b8a728e2290a376
ochukochris <chrisochuko101@gmail.com>
2020-07-17 10:14:57 +0100

    save

--- handler.go
+++ handler.go
@@ -3,17 +3,65 @@ package main
 import (
 	"fmt"
 
+	r "github.com/dancannon/gorethink"
 	"github.com/mitchellh/mapstructure"
+)
+
+const (
+	ChannelStop = iota
+	UserStop
+	MessageStop
 )
 
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
-	var message Message
-	mapstructure.Decode(data, &channel)
-	fmt.Printf("%#v\n", channel)
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
 
-	channel.Id = "1234"
-	message.Name = "channel add"
-	message.Data = channel
-	client.send <- message
+	go func() {
+		err = r.Table("channel").Insert(channel).Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeChannel(client *Client, data interface{}) {
+	stop := client.NewStopChannel(ChannelStop)
+	result := make(chan r.ChangeResponse)
+
+	cursor, err := r.Table("channel").Changes(r.ChangesOpts{IncludeInitial: true}).Run(client.session)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+
+	go func() {
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			result <- change
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-stop:
+				cursor.Close()
+				return
+			case change := <-result:
+				if change.NewValue != nil && change.OldValue == nil {
+					client.send <- Message{"channel add", change.NewValue}
+					fmt.Println("sent channel message")
+				}
+			}
+		}
+	}()
+}
+
+func unsubscribeChannel(client *Client, data interface{}) {
+	client.StopForKey(ChannelStop)
 }
